f35f5b75ec84cf5a252fcb0ced235584d81f6188
qyy891878034 <qyy891878034@163.com>
2020-06-11 12:03:49 +0800

    Initial commit

---
+++ deploy-burstkit4j.bat
@@ -0,0 +1,1 @@
+mvn deploy:deploy-file -DgroupId=com.liuqi -DartifactId=apc-burstkit4j -Dversion=v0.0.1 -Dfile=./target/apc-burstkit4j-0.0.1-SNAPSHOT.jar -DrepositoryId=snapshots -Durl=http://47.111.238.195:8081/repository/liuqi/
